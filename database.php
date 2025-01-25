fac96f0d6ef7bbeccdecc9fabbde8ed0baee5048
Ammar-majed <s444001188@uqu.edu.sa>
2025-01-25 15:09:59 +0300

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 // الاتصال بقاعدة البيانات
 $dsn = "mysql:host=localhost;dbname=dlms;charset=utf8mb4";
 $username = "root";
-$password = "Ammar999?";
+$password = "";
 $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
 try {
