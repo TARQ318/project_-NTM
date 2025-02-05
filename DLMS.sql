e7ba9953a749a9a194ce3ebd406c5e277abc7eaf
Ammar-majed <s444001188@uqu.edu.sa>
2025-02-05 10:28:17 +0300

    Update DLMS.sql

--- DLMS.sql
+++ DLMS.sql
@@ -38,5 +38,16 @@ CREATE TABLE borrows (
 );
 
 
+CREATE TABLE librarians (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'librarian',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
 ALTER TABLE students ADD COLUMN password VARCHAR(255) NOT NULL;
 
